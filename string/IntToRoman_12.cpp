d30e6cb75e75b7996299e7994e3d99a1ec464cf7
TopHK <TopShaojie.Kang@gmail.com>
2018-04-05 18:02:06 +0800

    add file

---
+++ string/IntToRoman_12.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<string>
+#include<unordered_map>
+#include<algorithm>
+using namespace std;
+
+int x=[](){
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}();
+
+class Solution
+{
+public:
+    string intToRoman(int num)
+    {
+        string romans;
+        vector<string> M = {"", "M", "MM", "MMM"};
+        vector<string> C = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        vector<string> X = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        vector<string> I = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+        return M[num/1000] + C[(num%1000)/100] + X[(num%100)/10] + I[num%10];
+    }
+};
+
+int main()
+{
+    Solution solution;
+    int num = 3001;
+    cout<<solution.intToRoman(num)<<endl;
+    return 0;
+}
+
+
+/*
+Given an integer, convert it to a roman numeral.
+
+Input is guaranteed to be within the range from 1 to 3999.
+*/
