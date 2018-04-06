f72d266d35bdbe4eb2edf5f0c107cc4c711eddc7
TopHK <TopShaojie.Kang@gmail.com>
2018-04-06 11:43:19 +0800

    add file

--- string/ExpressiveWords_809.cpp
+++ string/ExpressiveWords_809.cpp
@@ -13,9 +13,29 @@ int x=[](){
 class Solution
 {
 public:
+    // »¬¶¯´°¿ÚËã·¨
     int expressiveWords(string S, vector<string>& words)
     {
-
+        int res = 0;
+        for (auto &w : words)
+        {
+            if (w.size() <= S.size())
+            {
+                int i, j;
+                for (i = 0, j = 0; j < S.size(); j++)
+                {
+                    if (i <= w.size() && w[i] == S[j]) // w[i] references to a null char when i reaches w.size()
+                        i++;
+                    else if (j > 0 && S[j] == S[j - 1] && j + 1 < S.size() && S[j] == S[j + 1]) // last, this and next
+                        j++;
+                    else if (!(j > 1 && S[j] == S[j - 1] && S[j] == S[j - 2])) // this and last 2 chars
+                        break;
+                }
+                if (i == w.size() && j == S.size()) // both pointers reach the end
+                    res++;
+            }
+        }
+        return res;
     }
 };
 
@@ -39,9 +59,9 @@ void printVector(const vector<string> &result)
 int main()
 {
     Solution solution;
-    string digits = "23";
-    vector<string> results = solution.letterCombinations(digits);
-    printVector(results);
+    string S = "heeellooo";
+    vector<string> words = {"hello", "hi", "helo"};
+    cout<<solution.expressiveWords(S, words)<<endl;
     return 0;
 }
 
